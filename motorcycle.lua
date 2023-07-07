c2067d61fd9bb9e868edb2f0390214adf9bb373c
Yabastar <138197982+Yabastar@users.noreply.github.com>
2023-07-06 23:44:37 -0500

    Create motorcycle.lua

---
+++ motorcycle.lua
@@ -0,0 +1,27 @@
+speaker = peripheral.wrap("right")
+c = 0
+
+function note(amount, pitch, time, note)
+	c = 0
+	while c < amount do
+		speaker.playSound(note, 100, pitch)
+		sleep(time)
+		c = c + 1
+	end
+end
+
+sound = "block.glass.place"
+amount = 100
+print("Revving up!")
+note(amount, 1.5, 0.05, sound)
+note(amount, 1.75, 0.04, sound)
+note(amount, 2, 0.035, sound)
+note(amount, 2.25, 0.03, sound)
+print("Redline")
+note((amount * 1.5), 2.5, 0.015, sound)
+print("Letting go of gas")
+note((amount / 2), 2.25, 0.05, sound)
+note((amount / 2), 2, 0.04, sound)
+note((amount / 2), 1.75, 0.035, sound)
+note((amount / 2), 1.5, 0.03, sound)
+print("Turned off")
